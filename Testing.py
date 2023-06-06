78c330d07447f36af6a68c05acafeb2574bf58d0
solmala <92184025+solmala@users.noreply.github.com>
2023-06-06 14:38:01 -0300

    Update Testing.py

--- Testing.py
+++ Testing.py
@@ -4,7 +4,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+from selenium.webdriver.common.action_chains import ActionChains
+import time 
 
 class TestTranslate:
     @pytest.fixture
@@ -76,3 +77,37 @@ class TestTranslate:
         input1 =self.driver.find_element(By.CSS_SELECTOR, 'textarea[jsname="BJE2fc"]')
         input1.send_keys("Hola Mundo, mi nombre es el destructor de mundos")
         assert '48' in span.get_attribute('innerHTML')  
+
+
+
+class TestYoutube:
+    #No anda
+    @pytest.fixture
+
+    def setup(self):
+        self.driver = webdriver.Edge()
+        yield 
+        self.driver.quit()
+    
+    def test_watchlist(self, setup):
+        wait = WebDriverWait(self.driver, 10)
+        self.driver.get("https://www.youtube.com")
+        self.driver.maximize_window()
+        button = self.driver.find_element(By.CSS_SELECTOR, 'a[class="yt-spec-button-shape-next yt-spec-button-shape-next--outline yt-spec-button-shape-next--call-to-action yt-spec-button-shape-next--size-m yt-spec-button-shape-next--icon-leading "]')
+        button.click()
+        wait.until(EC.url_changes)
+        input = self.driver.find_element(By.CSS_SELECTOR, 'input[type="email"]')
+        input.send_keys('credenciales validas')
+        button = self.driver.find_element(By.CSS_SELECTOR, 'button[class="VfPpkd-LgbsSe VfPpkd-LgbsSe-OWXEXe-k8QpJ VfPpkd-LgbsSe-OWXEXe-dgl2Hf nCP5yc AjY5Oe DuMIQc LQeN7 qIypjc TrZEUc lw1w4b"]')
+        button.click()
+        input = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'input[type="password"]')))
+        input.send_keys('credenciales validas')
+        button = self.driver.find_element(By.CSS_SELECTOR, 'button[class="VfPpkd-LgbsSe VfPpkd-LgbsSe-OWXEXe-k8QpJ VfPpkd-LgbsSe-OWXEXe-dgl2Hf nCP5yc AjY5Oe DuMIQc LQeN7 qIypjc TrZEUc lw1w4b"]')
+        button.click()
+        time.sleep(15)
+        video = self.driver.find_element(By.ID, 'thumbnail')
+        ActionChains(self.driver).move_to_element(video)
+        button = self.driver.find_element(By.ID, 'button')
+        button.click()
+        button = self.driver.find_element(By.CSS_SELECTOR, 'ytd-menu-service-item-renderer[class="style-scope ytd-menu-popup-renderer"]')
+        button.click()
